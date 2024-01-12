824c945e3d6a9508cace5d84a64d44862d88e82b
HUSEYINTUNGA <95682830+HUSEYINTUNGA@users.noreply.github.com>
2024-01-12 20:55:57 +0300

    C# ile yazılmış Recursive Faktoriyel Programı
    Özyineleme denince ilk akla gelen örnek (Özyinelemeli Faktöriyek Hesabı)

---
+++ RecursiveFaktoriyel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_for_Faktoriyel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faktöriyelini hesapmalak istediğiniz değeri giriniz");
+            double n=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(n + "' in faktöriyeli = " + Faktoriyel(n));
+            Console.ReadKey();
+        }
+        public static double Faktoriyel(double n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n*Faktoriyel(n-1);
+            }
+        }
+    }
+}
+//H.TNG
